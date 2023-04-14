906ff06a1337ae837e2509719b8d6bd858f745eb
Noir <vokhuong1403@gmail.com>
2023-04-14 20:52:23 +0700

    Xong bai 40

--- src/main/java/com/exam/service/QuizService.java
+++ src/main/java/com/exam/service/QuizService.java
@@ -2,7 +2,6 @@ package com.exam.service;
 
 import com.exam.models.exam.Category;
 import com.exam.models.exam.Quiz;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Set;
@@ -15,4 +14,6 @@ public interface QuizService {
     public Quiz getQuiz(Long quizId);
     public void deleteQuiz(long quizId);
     public List<Quiz> getQuizzesOfCategory(Category category);
+    public List<Quiz> getActiveQuizzes();
+    public List<Quiz> getActiveQuizzesOfCategory(Category c);
 }
